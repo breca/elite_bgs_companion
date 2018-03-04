f5a285f764b1c8f7eee9708b0186276345108fe9
knosis <knosis@DEX>
2018-03-04 12:14:34 +1100

    added links for RS discord, github

--- lib/links.py
+++ lib/links.py
@@ -15,3 +15,18 @@ def donate():
 def skin():
     log.info('Opening donation link.')
     webbrowser.open_new(r"https://www.radiosidewinder.com/shipskins/")
+
+# feature request
+def feature_request():
+    log.info('Opening Github for a feature request.')
+    webbrowser.open_new(r"https://github.com/breca/elite_bgs_companion/issues")
+
+# feature request
+def bug_report():
+    log.info('Opening Github for a bug report.')
+    webbrowser.open_new(r"https://github.com/breca/elite_bgs_companion/issues")
+
+# feature request
+def discord():
+    log.info('Opening Discord link.')
+    webbrowser.open_new(r"https://discord.gg/0idLQikkQy6csbZx")
