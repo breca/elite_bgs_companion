c1ff53509af6110b90a7e91a675b55bf41f9a4c1
knosis <knosis@DEX>
2018-03-05 21:11:22 +1100

    Working, still needs journal handler for actual BGS data

--- lib/eddn_sender.py
+++ lib/eddn_sender.py
@@ -12,6 +12,7 @@ from time import sleep
 
 class EDDN_dispatcher(threading.Thread):
     def __init__(self, journal_path, config):
+        log.info('Initialsing EDDN dispatcher.')
         super(EDDN_dispatcher, self).__init__()
         self._stop_event = threading.Event()
         self.bgs_version = config['General']['version']
@@ -21,6 +22,8 @@ class EDDN_dispatcher(threading.Thread):
         try:
             while not self.stopped():
                 self.shipyard_monitor()
+                self.outfitting_monitor()
+                self.market_monitor()
                 sleep(1)
         except Exception as e:
             log.exception('EDDN Dispatcher exception during runtime.', e)
@@ -32,12 +35,24 @@ class EDDN_dispatcher(threading.Thread):
     def stopped(self):
         return self._stop_event.is_set()
 
-    def receive_response(self, response):
+    def receive_response(self, _, response):
         try:
-            print(str(response)) ##DEBUG
-            log.info('Recieved response from EDDN: {}'.format(str(response)))
+            if response.ok:
+                log.info('EDDN Dispatcher successfully posted data.')
+            else:
+                log.warning('EDDN Dispatcher recieved a bad response from posted data.')
+                log.warning(str(response.reason))
+                log.warning(str(response))
+
         except Exception as e:
             log.exception('EDDN Dispatcher errored.. somehow.. getting a response.', e)
+
+    '''-------------------------------------------------------------------------
+
+    File pollers - Here we check freshness of data in stored .json files and
+    send payloads to EDDN as required, with care to *try* and not send dupes.
+
+    -------------------------------------------------------------------------'''
 
     def shipyard_monitor(self):
         try:
@@ -45,9 +60,7 @@ class EDDN_dispatcher(threading.Thread):
             with open(filename, 'r') as f:
                 _list = f.readlines() # Frontier decided to put newlines everywhere so we get a list
                 _lines = ''.join(_list)
-                #log.debug('LINES: \n{}'.format(str(_lines))) #DEBUG
                 _json = json.loads(_lines)
-                #print(str(_json)) #DEBUG
 
             # see how stale this timestamp is
             timestamp_now = datetime.utcnow()
@@ -56,13 +69,10 @@ class EDDN_dispatcher(threading.Thread):
             # if there's more than 30 seconds between them, ignore
             timestamp_drift = timestamp_now - timestamp_file
 
-            #print(str(timestamp_drift.total_seconds())) #DEBUG
             log.debug('EDDN Dispatcher calculated data timestamp drift between json and realtime for Shipyard.json: {}'.format(timestamp_drift.total_seconds())) #DEBUG
             if not timestamp_drift.total_seconds() > 5:
-            #if timestamp_drift.total_seconds(): ##DEBUG FIXME
                 # Check to see if this data matches data previously stored in this class
                 if hasattr(self, 'old_shipyard_lines'):
-                    #print(str(self.old_shipyard_lines))
                     if self.old_shipyard_lines == _json:
                         log.debug('EDDN Dispatcher has already processed this Shipyard.json. Ignoring.')
                         return
@@ -78,6 +88,82 @@ class EDDN_dispatcher(threading.Thread):
             pass
 
 
+    # DID YOU JUST ACCUSE ME OF COPYING AND PASTING BECAUSE I WAS TOO LAZY TO WRITE A SMALL FUNCTION?!
+    def outfitting_monitor(self):
+        try:
+            filename = max(glob.iglob(path.join(self.journal_path, 'Outfitting.json')), key=path.getctime)
+            with open(filename, 'r') as f:
+                _list = f.readlines() # Frontier decided to put newlines everywhere so we get a list
+                _lines = ''.join(_list)
+                _json = json.loads(_lines)
+
+            # see how stale this timestamp is
+            timestamp_now = datetime.utcnow()
+            timestamp_file = datetime.strptime(_json['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
+            # subtract now from timestamp in file
+            # if there's more than 30 seconds between them, ignore
+            timestamp_drift = timestamp_now - timestamp_file
+
+            log.debug('EDDN Dispatcher calculated data timestamp drift between json and realtime for Outfitting.json: {}'.format(timestamp_drift.total_seconds())) #DEBUG
+            if not timestamp_drift.total_seconds() > 5:
+                # Check to see if this data matches data previously stored in this class
+                if hasattr(self, 'old_outfitting_lines'):
+                    if self.old_outfitting_lines == _json:
+                        log.debug('EDDN Dispatcher has already processed this Outfitting.json. Ignoring.')
+                        return
+                else:
+                    # We'll store the json, just to try and ensure we don't double up or spam data
+                    self.old_outfitting_lines = _json
+                    # Construct and send the payload
+                    self.send_outfitting(_json)
+            else:
+                log.debug('EDDN Dispatcher found stale Outfitting data.')
+        except Exception as e:
+            log.exception('EDDN Dispatcher encountered error trying to post Outfitting data.', e)
+            pass
+
+
+    # DID YOU JUST ACCUSE ME OF COPYING AND PASTING BECAUSE I WAS TOO LAZY TO WRITE A SMALL FUNCTION?! AGAIN?!
+    def market_monitor(self):
+        try:
+            filename = max(glob.iglob(path.join(self.journal_path, 'Market.json')), key=path.getctime)
+            with open(filename, 'r') as f:
+                _list = f.readlines() # Frontier decided to put newlines everywhere so we get a list
+                _lines = ''.join(_list)
+                _json = json.loads(_lines)
+
+            # see how stale this timestamp is
+            timestamp_now = datetime.utcnow()
+            timestamp_file = datetime.strptime(_json['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
+            # subtract now from timestamp in file
+            # if there's more than 30 seconds between them, ignore
+            timestamp_drift = timestamp_now - timestamp_file
+
+            log.debug('EDDN Dispatcher calculated data timestamp drift between json and realtime for Market.json: {}'.format(timestamp_drift.total_seconds())) #DEBUG
+            if not timestamp_drift.total_seconds() > 5:
+                # Check to see if this data matches data previously stored in this class
+                if hasattr(self, 'old_market_lines'):
+                    if self.old_market_lines == _json:
+                        log.debug('EDDN Dispatcher has already processed this Market.json. Ignoring.')
+                        return
+                else:
+                    # We'll store the json, just to try and ensure we don't double up or spam data
+                    self.old_market_lines = _json
+                    # Construct and send the payload
+                    self.send_market(_json)
+            else:
+                log.debug('EDDN Dispatcher found stale Market data.')
+        except Exception as e:
+            log.exception('EDDN Dispatcher encountered error trying to post Market data.', e)
+            pass
+
+
+    '''-------------------------------------------------------------------------
+
+    POST functions  .... for posting. To EDDN.
+
+    -------------------------------------------------------------------------'''
+
     def send_shipyard(self, data):
         session = FuturesSession()
         log.info('EDDN Dispatcher sending Shipyard payload.')
@@ -86,7 +172,7 @@ class EDDN_dispatcher(threading.Thread):
         payload = {
           "$schemaRef": "https://eddn.edcd.io/schemas/shipyard/2/test",
           "header": {
-            "uploaderID": "Kalis",
+            "uploaderID": "Anon",
             "softwareName": "BGS Companion",
             "softwareVersion": self.bgs_version
           },
@@ -102,87 +188,78 @@ class EDDN_dispatcher(threading.Thread):
             payload['message']['ships'].append(ship['ShipType'])
 
         payload = json.dumps(payload)
-        log.debug('EDDN Dispatcher Final Shipyard Payload: {}'.format(str(payload)))
+        log.debug('EDDN Dispatcher Shipyard Data Payload: {}'.format(str(payload)))
         r = session.post('https://eddn.edcd.io:4430/upload/', data=payload, background_callback=self.receive_response)
-        #r = session.post('https://wiki.hiteklolife.net', data=payload, background_callback=self.receive_response)
         r.result()
 
-        # def outfitting(self, data):
-        #     try:
-        #         session = FuturesSession()
-        #         payload = {
-        #           "$schemaRef": "https://eddn.edcd.io/schemas/outfitting/2",
-        #           "header": {
-        #             "uploaderID": "Anon",
-        #             "softwareName": "BGS Companion",
-        #             "softwareVersion": self.bgs_version
-        #           },
-        #           "message": {
-        #             "systemName": data['StationName'],
-        #             "stationName": data['StationName'],
-        #             "timestamp": data['timestamp'],
-        #             "modules": []
-        #           }
-        #         }
-        #
-        #         for module in data['Items']:
-        #             payload['message']['modules'].append(data['Name'])
-        #
-        #         r = session.post('https://eddn.edcd.io:4430/upload/', data=payload, background_callback=self.receive_response)
-        #         r.result()
-        #     except Exception as e:
-        #         log.exception('Exception occured sending outfitting payload to EDDN', e)
-        #         pass
+
+    def send_outfitting(self, data):
+        session = FuturesSession()
+        log.info('EDDN Dispatcher sending Outfitting payload.')
+        log.debug('EDDN Dispatcher Raw Outfitting Data: {}'.format(str(data)))
+
+        payload = {
+          "$schemaRef": "https://eddn.edcd.io/schemas/outfitting/2/test",
+          "header": {
+            "uploaderID": "Anon",
+            "softwareName": "BGS Companion",
+            "softwareVersion": self.bgs_version
+          },
+          "message": {
+            "systemName": data['StarSystem'],
+            "stationName": data['StationName'],
+            "timestamp": data['timestamp'],
+            "modules": []
+          }
+        }
+
+        for module in data['Items']:
+            if 'PlanetApproachSuite' not in module['Name'] and 'sku' not in module['Name']:
+                if module['Name'].lower().startswith('hpt_') or module['Name'].lower().startswith('int_') or '_armour_' in module['Name'].lower():
+                    payload['message']['modules'].append(module['Name'].title()) #Have to add title() due to EDDN regex filter
+
+        payload = json.dumps(payload)
+        log.debug('EDDN Dispatcher Outfitting Data Payload: {}'.format(str(payload)))
+        r = session.post('https://eddn.edcd.io:4430/upload/', data=payload, background_callback=self.receive_response)
+        r.result()
 
 
-    # def outfitting_monitor(self):
-    #     outfit = max(glob.iglob(path.join(journal_path, 'Outfitting.json'), key=path.getctime))
-    #     with open(outfit, 'r') as f:
-    #         outfit_lines = f.readlines()
-    #     if not hasattr(self, 'old_outfit_lines'):
-    #         log.info('EDDN Dispatcher found new Outfitting file.')
-    #         self.old_outfit_lines = outfit_lines
-    #     else:
-    #         if self.old_outfit_lines != outfit_lines:
-    #             self.payload.outfitting(old_outfit_lines)
-    #             self.old_outfit_lines = outfit_lines
-    #
-    #
-    # def market_monitor(self):
+    def send_market(self, data):
+        session = FuturesSession()
+        log.info('EDDN Dispatcher sending Market payload.')
+        log.debug('EDDN Dispatcher Raw Market Data: {}'.format(str(data)))
 
+        payload = {
+          "$schemaRef": "https://eddn.edcd.io/schemas/commodity/3/test",
+          "header": {
+            "uploaderID": "Anon",
+            "softwareName": "BGS Companion",
+            "softwareVersion": self.bgs_version
+          },
+          "message": {
+            "systemName": data['StarSystem'],
+            "stationName": data['StationName'],
+            "timestamp": data['timestamp'],
+            "commodities": []
+          }
+        }
 
+        for commodity in data['Items']:
+            c_dict = { 'name': commodity['Name'],
+                       'meanPrice': commodity['MeanPrice'],
+                       'buyPrice': commodity['BuyPrice'],
+                       'stock': commodity['Stock'],
+                       'stockBracket': commodity['StockBracket'],
+                       'sellPrice': commodity['SellPrice'],
+                       'demand': commodity['Demand'],
+                       'demandBracket': commodity['DemandBracket']
+                       }
 
+        for commodity in data['Items']:
+            if 'legality' not in commodity.keys(): ##REVIEW
+                    payload['message']['commodities'].append(c_dict)
 
-
-        # def market(self, data):
-        #     try:
-        #         payload = {
-        #           "$schemaRef": "https://eddn.edcd.io/schemas/commodity/3",
-        #           "header": {
-        #             "uploaderID": "Anon",
-        #             "softwareName": "BGS Companion",
-        #             "softwareVersion": config['General']['Version']
-        #           },
-        #           "message": {
-        #             "systemName": data['StationName'],
-        #             "stationName": data['StationName'],
-        #             "timestamp": data['timestamp'],
-        #             "commodities": []
-        #           }
-        #         }
-        #
-        #         for commodity in data['Items']:
-        #             c_dict = { 'name': commodity['Name'],
-        #                        'meanPrice': commodity['MeanPrice'],
-        #                        'buyPrice': commodity['BuyPrice'],
-        #                        'stock': commodity['Stock'],
-        #                        'stockBracket': commodity['StockBracket'],
-        #                        'sellPrice': commodity['SellPrice'],
-        #                        'demand': commodity['Demand'],
-        #                        'demandBracket': commodity['DemandBracket']
-        #                        }
-        #             payload['commodities'].append(c_dict)
-        #     except Exception as e:
-        #         log.exception('Exception occured sending market payload to EDDN', e)
-        #         pass
-        #
+        payload = json.dumps(payload)
+        log.debug('EDDN Dispatcher Market Data Payload: {}'.format(str(payload)))
+        r = session.post('https://eddn.edcd.io:4430/upload/', data=payload, background_callback=self.receive_response)
+        r.result()
