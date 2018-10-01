be7ac85dabb91b17d11b05647b51fe1239a9ef38
breca <32656963+breca@users.noreply.github.com>
2018-10-01 10:46:35 +1000

    formatting, tweaked clocks, added crime window

--- windows/main.py
+++ windows/main.py
@@ -4,6 +4,8 @@ from PIL import ImageTk, Image
 from datetime import datetime, timedelta
 from . import settings
 from . import bgs
+from . import timezone
+from . import crime
 
 import logging
 logger = logging.getLogger(__name__)
@@ -14,17 +16,19 @@ class MainWindow(Tk):
 
     def __init__(self, config): #, q_main, q_db):
         logging.debug('Creating main window.')
+        self.conf = config
 
         # Create window
         Tk.__init__(self)
 
-        self.title(config['theme']['titles']['main'])
+        self.title(self.conf['theme']['titles']['main'])
         self.geometry("610x340")
-        self.iconbitmap(config['theme']['icon'])
+        self.iconbitmap(self.conf['theme']['icon'])
         self.resizable(0,0)  # Disable resize
         self.rowconfigure(13, pad=10)
         self.columnconfigure(2, weight=5)
         self.protocol("WM_DELETE_WINDOW", self.shutdown)
+
         # self.queue = queue.Queue()
         # self.queue = q_main
         # self.q_db = q_db
@@ -58,59 +62,59 @@ class MainWindow(Tk):
         self.menu_main = Menu(self.menu_bar, tearoff=0)
 
         # This section includes/excludes menu features based on menus.yml. It's a bit messy. :(
-        if config['menu']['main']['settings']:
-            self.menu_main.add_command(label="Settings", command=lambda: settings.SettingsWindow(self, config))
+        if self.conf['menu']['main']['settings']:
+            self.menu_main.add_command(label="Settings", command=lambda: settings.SettingsWindow(self))
         self.menu_main.add_command(label="Check for updates", command=stub)
-        if config['menu']['main']['feature_request'] or config['menu']['bug_report']:
+        if self.conf['menu']['main']['feature_request'] or self.conf['menu']['bug_report']:
             self.menu_main.add_separator()
-        if config['menu']['main']['feature_request']:
+        if self.conf['menu']['main']['feature_request']:
             self.menu_main.add_command(label="Request a feature", command=stub)
-        if config['menu']['main']['bug_report']:
+        if self.conf['menu']['main']['bug_report']:
             self.menu_main.add_command(label="Report a bug", command=stub)
-        if config['menu']['main']['credits']:
+        if self.conf['menu']['main']['credits']:
             self.menu_main.add_separator()
             self.menu_main.add_command(label="Credits", command=stub)
-        if config['menu']['main']:
+        if self.conf['menu']['main']:
             self.menu_main.add_separator()
             self.menu_main.add_command(label="Exit", command=self.shutdown)
 
         # Add menus to menu bar
         self.menu_bar.add_cascade(label="Menu", menu=self.menu_main)
-        if config['menu']['timezones']:
-            self.menu_bar.add_command(label="Timezones", command=stub)
-        if config['menu']['radio']:
-            self.menu_bar.add_command(label=config['theme']['menu']['radio'], command=stub)
-        if config['menu']['affiliate']:
-            self.menu_bar.add_command(label=config['theme']['menu']['affiliate'], command=stub)
-        if config['menu']['donate']:
-            self.menu_bar.add_command(label=config['theme']['menu']['donate'], command=stub)
+        if self.conf['menu']['timezones']:
+            self.menu_bar.add_command(label="Timezones", command=lambda: timezone.TZWindow(self))
+        if self.conf['menu']['radio']:
+            self.menu_bar.add_command(label=self.conf['theme']['menu']['radio'], command=stub)
+        if self.conf['menu']['affiliate']:
+            self.menu_bar.add_command(label=self.conf['theme']['menu']['affiliate'], command=stub)
+        if self.conf['menu']['donate']:
+            self.menu_bar.add_command(label=self.conf['theme']['menu']['donate'], command=stub)
 
         # Faction image
-        self.faction_image_file = ImageTk.PhotoImage(Image.open(config['theme']['logo']))
+        self.faction_image_file = ImageTk.PhotoImage(Image.open(self.conf['theme']['logo']))
         self.faction_image = Label(self, image=self.faction_image_file)
         self.faction_image.grid(column=0, row=0, rowspan=9, columnspan=2, sticky=N)
 
         # Local clock
-        if config['menu']['clock_local']:
+        if self.conf['menu']['clock_local']:
             self.text_clock_localtime = Label(self, text='Local time:')
             self.text_clock_localtime.grid(column=0, row=9, sticky=E)
             self.clock_localtime = Label(self, textvariable=self.clock_local)
             self.clock_localtime.grid(column=1, row=9, sticky=W)
 
         # Server clock
-        if config['menu']['clock_server']:
+        if self.conf['menu']['clock_server']:
             self.text_clock_servertime = Label(self, text='Server time:')
             self.text_clock_servertime.grid(column=0, row=10, sticky=E)
             self.clock_servertime = Label(self, textvariable=self.clock_server)
             self.clock_servertime.grid(column=1, row=10, sticky=W)
 
         # Board countdown
-        if config['menu']['countdown_board']:
+        if self.conf['menu']['countdown_board']:
             self.clock_countdown_board = Label(self, textvariable=self.countdown_board)
             self.clock_countdown_board.grid(column=0, columnspan=2, row=11, sticky=W+E)
 
         # Influence tick countdown
-        if config['menu']['countdown_influence']:
+        if self.conf['menu']['countdown_influence']:
             self.clock_countdown_influence = Label(self, textvariable=self.countdown_influence)
             self.clock_countdown_influence.grid(column=0, columnspan=2, row=12, sticky=W+E)
 
@@ -162,7 +166,7 @@ class MainWindow(Tk):
         self.session_stats_mode.grid_propagate(False)
 
         # Advisor
-        if config['settings']['show_advisor']:
+        if self.conf['settings']['show_advisor']:
             self.text_advisor = Label(self.frame_session, justify=LEFT, text='Advisor:')
             self.text_advisor.grid(column=0, row=10, sticky=E)
             self.advisor_label = Label(self.frame_session, justify=LEFT, textvariable=self.advisor, wraplength=300)
@@ -185,11 +189,18 @@ class MainWindow(Tk):
 
         # Arbitrarily stick button on there somewhere
         self.buttons_frame = ttk.Frame(self, padding="3 3 3 3")
-        self.buttons_frame.grid(sticky="N,E,S,W")
+        self.buttons_frame.grid(sticky="E,S,W")
         self.buttons_frame.rowconfigure(0)
-        self.buttons_frame.place(y=230, x=510)
+        self.buttons_frame.place(y=230, x=410)
 
-        self.button_view_bgs = ttk.Button(self.buttons_frame, text='View BGS Stats', command=lambda: bgs.BgsWindow(self, config))
+        self.button_view_bgs = ttk.Button(
+            self.buttons_frame, text='View BGS Stats', command=lambda: bgs.BgsWindow(self)
+        )
+        self.button_view_bgs.grid(column=2, row=3)
+
+        self.button_view_bgs = ttk.Button(
+            self.buttons_frame, text='View Crime Report', command=lambda: crime.CrimeWindow(self)
+        )
         self.button_view_bgs.grid(column=1, row=3)
 
         self.update_clock_times()
@@ -289,39 +300,52 @@ class MainWindow(Tk):
 
     def update_clock_times(self):
         """ Update clocks and countdowns """
-        now = datetime.now()
-        utc = datetime.utcnow()
+        try:
+            now = datetime.now()
+            utc = datetime.utcnow()
 
-        # Update clocks
-        self.clock_local.set(datetime.strftime(now, "%H:%M:%S"))
-        self.clock_server.set(datetime.strftime(utc, "%H:%M:%S"))
+            # Update clocks
+            self.clock_local.set(datetime.strftime(now, "%H:%M:%S"))
+            self.clock_server.set(datetime.strftime(utc, "%H:%M:%S"))
 
-        ''' 
-        Update influence countdown.
-        We have no official word as to when this window actually occurs
-        but the following is derived from community testing and is generally accepted
-        as the "right" answer. This fuzzyness allows the developer to get /real/ lazy
-        with his time calculations. 
-        '''
-        influence_processing_start = utc.replace(hour=12, minute=0, second=0)
-        influence_processing_end = utc.replace(hour=13, minute=0, second=0)
+            # Update influence countdown.
+            influence_processing_start = utc.replace(
+                hour=datetime.strptime(self.conf['bgs']['inf_tick_start'], '%H:%M').hour,
+                minute=datetime.strptime(self.conf['bgs']['inf_tick_start'], '%H:%M').minute
+            )
+            influence_processing_end = utc.replace(
+                hour=datetime.strptime(self.conf['bgs']['inf_tick_end'], '%H:%M').hour,
+                minute=datetime.strptime(self.conf['bgs']['inf_tick_end'], '%H:%M').minute
+            )
 
-        if utc > influence_processing_end:  # Influence has already been processed today, calc time until tomorrow
-            influence_processing_start = influence_processing_start.replace(day=influence_processing_start.day+1)
-        delta = utc - influence_processing_start
+            if utc > influence_processing_end:  # Influence has already been processed today, calc time until tomorrow
+                try:
+                    influence_processing_start = influence_processing_start.replace(day=influence_processing_start.day + 1)
+                except ValueError:
+                    influence_processing_start = influence_processing_start.replace(
+                        day=1,
+                        month=influence_processing_start.month + 1
+                    )
+                    pass
 
-        if utc < influence_processing_start: # Influence window has yet to occur
-            remaining_hours = delta.seconds / 3600
-            if remaining_hours >= 1.2:
-                self.countdown_influence.set('Influence tick begins in around {} hours.'.format(str(remaining_hours)[0:2]))
-            elif remaining_hours < 1.2 and remaining_hours > 1:
-                self.countdown_influence.set('Influence tick begins in around an hour.')
-            elif remaining_hours < 1 and remaining_hours > 0.5:
-                self.countdown_influence.set('Influence tick begins in less than an hour.')
-            elif remaining_hours < 0.15:
-                self.countdown_influence.set('Influence tick begins shortly.')
-        elif utc > influence_processing_start and utc < influence_processing_end: # Influence is calculating now
-            self.countdown_influence.set('Influence is currently being calculated.')
+            delta = influence_processing_start - utc
+
+            if utc < influence_processing_start: # Influence window has yet to occur
+                remaining_hours = delta.seconds / 3600
+                if remaining_hours >= 1.2:
+                    self.countdown_influence.set('Influence tick begins in around {} hours.'.format(str(remaining_hours)[0:2]))
+                elif remaining_hours < 1.2 and remaining_hours > 1:
+                    self.countdown_influence.set('Influence tick begins in around an hour.')
+                elif remaining_hours < 1 and remaining_hours > 0.5:
+                    self.countdown_influence.set('Influence tick begins in less than an hour.')
+                elif remaining_hours < 0.15:
+                    self.countdown_influence.set('Influence tick begins shortly.')
+            elif utc > influence_processing_start and utc < influence_processing_end: # Influence is calculating now
+                self.countdown_influence.set('Influence is currently being calculated.')
+        except:
+            logging.exception('Caught exception during influence tick.')
+            self.countdown_influence.set('Time until influence tick unknown.')
+            pass
 
         ''' 
         Update mission board countdown (for board flippers).
@@ -335,53 +359,58 @@ class MainWindow(Tk):
         (Yes, I'm bad at math - BC)
         '''
 
-        discard = timedelta(minutes=utc.minute % 15,
-                            seconds=utc.second,
-                            microseconds=utc.microsecond)
-        # Get time until next interval
-        rounded = utc - discard
-        if discard <= timedelta(minutes=15):
-            rounded += timedelta(minutes=15)
-        final = rounded - utc
-        seconds_total = final.total_seconds()
-        minutes = int((seconds_total % 3600) / 60)
-        seconds = int(seconds_total % 60)
+        try:
+            discard = timedelta(minutes=utc.minute % 15,
+                                seconds=utc.second,
+                                microseconds=utc.microsecond)
 
-        if minutes > 1:
-            self.countdown_board.set('Boards refresh in {} minutes.'.format(minutes))
-        if minutes == 1:
-            self.countdown_board.set('Boards refresh in {} minute, {} seconds.'.format(minutes, seconds))
-        elif minutes < 1:
-            self.countdown_board.set('Boards refresh in {} seconds.'.format(seconds))
+            # Get time until next interval
+            rounded = utc - discard
+            if discard <= timedelta(minutes=15):
+                rounded += timedelta(minutes=15)
+            final = rounded - utc
+            seconds_total = final.total_seconds()
+            minutes = int((seconds_total % 3600) / 60)
+            seconds = int(seconds_total % 60)
+
+            if minutes > 1:
+                self.countdown_board.set('Boards refresh in {} minutes.'.format(minutes))
+            if minutes == 1:
+                self.countdown_board.set('Boards refresh in {} minute, {} seconds.'.format(minutes, seconds))
+            elif minutes < 1:
+                self.countdown_board.set('Boards refresh in {} seconds.'.format(seconds))
+        except:
+            logging.exception('Exception occured updating board refresh.')
+            self.countdown_board.set('Time until board refresh unknown.')
 
         self.after(1000, self.update_clock_times)
 
-    def monitor_queue(self):
-        """
-        This monitors the queue and executes actions that update the GUI when certain conditions are met.
-        Predominately this is just updating elements like the status bar.
-        """
-        try:
-            item = self.queue.get(block=False)
-        except:  # queue.Empty:
-            pass
-        else:
-            try:
-                if item['type'] == 'name':
-                    self.update_commander(item['value'])
-                elif item['type'] == 'credits':
-                    self.update_credits(item['value'])
-                elif item['type'] == 'mode':
-                    self.update_game_mode(item['value'])
-                elif item['type'] == 'location':
-                    self.update_location(item['value'])
-                elif item['type'] == 'advisor':
-                    self.update_advisor(item['value'])
-            except KeyError:
-                logging.warning('Window queue received a packet that it doesn\'t subscribe to: ' + str(item))
-                pass
-
-        self.after(100, self.monitor_queue)
+    # def monitor_queue(self):
+    #     """
+    #     This monitors the queue and executes actions that update the GUI when certain conditions are met.
+    #     Predominately this is just updating elements like the status bar.
+    #     """
+    #     try:
+    #         item = self.queue.get(block=False)
+    #     except:  # queue.Empty:
+    #         pass
+    #     else:
+    #         try:
+    #             if item['type'] == 'name':
+    #                 self.update_commander(item['value'])
+    #             elif item['type'] == 'credits':
+    #                 self.update_credits(item['value'])
+    #             elif item['type'] == 'mode':
+    #                 self.update_game_mode(item['value'])
+    #             elif item['type'] == 'location':
+    #                 self.update_location(item['value'])
+    #             elif item['type'] == 'advisor':
+    #                 self.update_advisor(item['value'])
+    #         except KeyError:
+    #             logging.warning('Window queue received a packet that it doesn\'t subscribe to: ' + str(item))
+    #             pass
+    #
+    #     self.after(100, self.monitor_queue)
 
 
 def stub():
